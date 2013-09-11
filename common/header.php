56202f268e9706f92ecaf7a06ef130f69886b2eb
swadm <swadm@pathology-archive.oit.umn.edu>
2013-09-11 12:48:16 -0500

    adding favicon

--- common/header.php
+++ common/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="<?php img("favicon.png") ?>" />
     <?php if ( $description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
